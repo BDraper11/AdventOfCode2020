0221e2d108e62b5d695455dae9cc47bf874721c1
BDraper11 <54155379+BDraper11@users.noreply.github.com>
2020-12-04 17:19:29 +0000

    Day4Init

---
+++ Day4Puzzle.py
@@ -0,0 +1,27 @@
+class IDDoc:
+    def __init__(self, block):
+        def GetBirthYear(self):
+            return 0
+        def GetIssueYear(self):
+            return 0
+        def GetExpiryYear(self):
+            return 0
+        def GetHeight(self):
+            return 0
+        def GetHairColour(self):
+            return 0
+        def GetEyeColour(self):
+            return 0
+        def GetPassportID(self):
+            return 0
+        def GetCountryID(self):
+            return 0
+
+        self.BirthYear = GetBirthYear(self)
+        self.IssueYear = GetIssueYear(self)
+        self.ExpiryYear = GetExpiryYear(self)
+        self.Height = GetHeight(self)
+        self.HairColour = GetHairColour(self)
+        self.EyeColour = GetEyeColour(self)
+        self.PassportID = GetPassportID(self)
+        self.CountryID = GetCountryID(self)
