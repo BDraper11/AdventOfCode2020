fc549ddf0fe5006f899b4edf85d81dd2f39d1245
BDraper11 <54155379+BDraper11@users.noreply.github.com>
2020-12-04 19:09:30 +0000

    Update Day4Puzzle.py
    Part1 Done

--- Day4Puzzle.py
+++ Day4Puzzle.py
@@ -1,27 +1,77 @@
+#%%
 class IDDoc:
     def __init__(self, block):
-        def GetBirthYear(self):
-            return 0
-        def GetIssueYear(self):
-            return 0
-        def GetExpiryYear(self):
-            return 0
-        def GetHeight(self):
-            return 0
-        def GetHairColour(self):
-            return 0
-        def GetEyeColour(self):
-            return 0
-        def GetPassportID(self):
-            return 0
-        def GetCountryID(self):
-            return 0
+        block = block.split()
 
-        self.BirthYear = GetBirthYear(self)
-        self.IssueYear = GetIssueYear(self)
-        self.ExpiryYear = GetExpiryYear(self)
-        self.Height = GetHeight(self)
-        self.HairColour = GetHairColour(self)
-        self.EyeColour = GetEyeColour(self)
-        self.PassportID = GetPassportID(self)
-        self.CountryID = GetCountryID(self)
+        def GetEntry(self,block,SearchString):
+            for entry in block:
+                if entry.find(SearchString)!=-1:
+                    EntryReturn = entry.split(":")[1]
+                    break
+                else:
+                    EntryReturn = ''
+            return EntryReturn
+        def GetBirthYear(self, block):
+            BirthYear = GetEntry(self,block,"byr")
+            return BirthYear
+        def GetIssueYear(self, block):
+            IssueYear = GetEntry(self,block,"iyr")
+            return IssueYear
+        def GetExpiryYear(self, block):
+            ExpiryYear = GetEntry(self,block,"eyr")
+            return ExpiryYear
+        def GetHeight(self, block):
+            Height = GetEntry(self,block,"hgt")
+            return Height
+        def GetHairColour(self, block):
+            HairColour = GetEntry(self,block,"hcl")
+            return HairColour
+        def GetEyeColour(self, block):
+            EyeColour = GetEntry(self,block,"ecl")
+            return EyeColour
+        def GetPassportID(self, block):
+            PassportID = GetEntry(self,block,"pid")
+            return PassportID
+        def GetCountryID(self, block):
+            CountryID = GetEntry(self,block,"cid")
+            return CountryID
+        def GetValidIDDocStatus(self):
+            ValidStatus = 1
+            for attribute, value in self.__dict__.items():
+                #print(attribute,value)
+                if attribute != "CountryID":
+                    if value == '':
+                        ValidStatus = 0
+            return ValidStatus
+
+        self.BirthYear = GetBirthYear(self, block)
+        self.IssueYear = GetIssueYear(self, block)
+        self.ExpiryYear = GetExpiryYear(self, block)
+        self.Height = GetHeight(self, block)
+        self.HairColour = GetHairColour(self, block)
+        self.EyeColour = GetEyeColour(self, block)
+        self.PassportID = GetPassportID(self, block)
+        self.CountryID = GetCountryID(self, block)
+        self.ValidIDDocStatus = GetValidIDDocStatus(self)
+
+def GetBlocks():
+    with open("Day4Data.txt") as f:
+        lines = f.read()
+        blocks = lines.split("\n\n")
+    return blocks
+
+blocks = GetBlocks()
+
+IDDocs = []
+for block in blocks:
+    IDDocs.append(IDDoc(block))
+
+CountValidIDDocs = 0
+for IDDoc in IDDocs:
+    if IDDoc.ValidIDDocStatus:
+        CountValidIDDocs += 1
+
+print("Done")
+print("Number of valid ID documents : ",CountValidIDDocs)
+
+# %%
