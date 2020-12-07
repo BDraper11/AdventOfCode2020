15786d73d562a4c4517160c224c74e9a97a46356
BDraper11 <54155379+BDraper11@users.noreply.github.com>
2020-12-07 16:26:21 +0000

    Day7Init Other lines of description....?

---
+++ Day7Puzzle.py
@@ -0,0 +1,11 @@
+'''
+1) Parse data by "bags contain" to get LHS Subject Bag & RHS Subject Bag Rules - Dictionary?
+2) 
+'''
+
+#%%
+x = list(["a","b","c","d"])
+y = list(["r","s","t"])
+z = list([x,y])
+print("b" in z)
+print("b" in x)
