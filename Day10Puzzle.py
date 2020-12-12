51341dc311f9cb3cd44521c3f432c9c15de6f20f
BDraper11 <54155379+BDraper11@users.noreply.github.com>
2020-12-12 09:51:41 +0000

    Day10 TBC

--- Day10Puzzle.py
+++ Day10Puzzle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 #%%
+import math
 from itertools import permutations
 def GetDataRaw(): # Get Raw Data.
     with open("Day10Data.txt") as f:
@@ -26,21 +27,41 @@ def GetPart1Ans(DiffArray):
     Ans = Count1*Count3
     return Ans
 
-def Part2():
+def Part2(Data,DiffArray):
+    def fact(n):
+        if (n <= 1):
+            return 1
+        return n * fact(n - 1) 
+    def nPr(n, r):
+        return math.floor(fact(n) / fact(n - r))
+
     StartIdx = 0
     EndIdx = 0
     RangeIdxs = list()
+
+    Data = Data[1:-1]
+    DiffArray = DiffArray[1:-1]
+    ## Determine blocks of data parsed by 3-gaps.
     for i,r in enumerate(DiffArray):
         if r == 3:
             EndIdx = i
             RangeIdxs.append([StartIdx,EndIdx])
             StartIdx = EndIdx+1
-    MaxDiff = 0
-    for r in RangeIdxs:
-        MaxDiff = max((r[1]-r[0]),MaxDiff)
-
-
-
+        if r !=3 and i+1 == len(DiffArray):
+            EndIdx = i+1
+            RangeIdxs.append([StartIdx,EndIdx])
+            StartIdx = EndIdx+1
+    ## Determine the maximum width before 3-gap
+    MaxRangeLength = 0
+    NumPerms = []
+    for n in RangeIdxs: #For each of the 3-gap separated groups.
+        RangeLength = (n[1]-n[0])+1
+        MaxRangeLength = max(RangeLength,MaxRangeLength)
+        for r in range(1,RangeLength):
+            print(r)
+            m = n[:r]
+            ThisPerm = permutations(m)
+            if ThisPerm
     return 0
 
 if __name__ == "__main__":
@@ -48,6 +69,25 @@ if __name__ == "__main__":
     Data = GetData(DataRaw)
     DiffArray = GetDiffArray()
     print(GetPart1Ans(DiffArray))
-    Part2()
+    Part2(Data,DiffArray)
 
 print("Done")
+#%%
+x = list(range(1,5+1))
+print(x)
+y = [2,3,4,5,6]
+z = y[:3]
+print(z)
+from collections import Counter  
+c = Counter({0:1})
+print (c)
+# %%
+from collections import Counter  
+data = sorted([int(x.strip()) for x in open("Day10Data.txt")] + [0])  
+c = Counter({0:1})  
+for x in data:  
+    c[x+1] += c[x]  
+    c[x+2] += c[x]  
+    c[x+3] += c[x] 
+print("Part 2:", c[max(data) + 3])
+# %%
